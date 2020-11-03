3e32948d3ce1d894b4c12c37af9f0d5917d53a98
kitty <renne@kitty.yuki>
2020-11-03 15:34:01 +0800

    [ADD]enhance golang package

--- config.sh
+++ config.sh
@@ -1,1 +1,12 @@
-echo "hello"
+#!/bin/bash
+
+cp sources.list /etc/apt
+apt-get update
+apt-get -f dist-upgrade
+apt-get install -y --allow-downgrades perl-base=5.26.1-6ubuntu0.5
+apt-get install -y perl liberror-perl
+apt-get install -y vim-tiny git net-tools wget
+
+wget https://golang.google.cn/dl/go1.14.linux-amd64.tar.gz go1.14.tar.gz
+
+export GOPROXY=https://goproxy.cn
